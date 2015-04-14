14936ad62f50d68d6b3c1269a8522120e8f3c174
Ying Xiong <yxiong@seas.harvard.edu>
2015-04-14 17:52:13 -0400

    Add latex preamble to use amsfonts

--- docs/conf.py
+++ docs/conf.py
@@ -200,7 +200,7 @@ latex_elements = {
 #'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-#'preamble': '',
+'preamble': r'\usepackage{amsfonts}',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
