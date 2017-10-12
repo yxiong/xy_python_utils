1961c0a5af881bf05e15d308401a8276a627c98f
Ying Xiong <yxiong@dropbox.com>
2017-10-12 13:54:16 -0700

    Add minorticks_on function to plt_utils

--- xy_python_utils/plt_utils.py
+++ xy_python_utils/plt_utils.py
@@ -4,6 +4,7 @@
 # Created: Jun 02, 2017.
 
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 import numpy as np
 
 from matplotlib.cbook import iterable
@@ -17,3 +18,26 @@ def histb(x, bins=None, **kargs):
         x[x < x_min] = x_min
         x[x > x_max] = x_max
     plt.hist(x, bins, **kargs)
+
+
+def minorticks_on(ax=None, which="both"):
+    """Same as ax.minorticks_on, but takes a `which` parameter specifying `x`, `y` or `both`."""
+    if ax == None:
+        ax = plt.gca()
+    if which == "both":
+        axes = [ax.xaxis, ax.yaxis]
+    elif which == "x":
+        axes = [ax.xaxis]
+    elif which == "y":
+        axes = [ax.yaxis]
+    for ax in axes:
+        scale = ax.get_scale()
+        if scale == 'log':
+            s = ax._scale
+            ax.set_minor_locator(mticker.LogLocator(s.base, s.subs))
+        elif scale == 'symlog':
+            s = ax._scale
+            ax.set_minor_locator(
+                mticker.SymmetricalLogLocator(s._transform, s.subs))
+        else:
+            ax.set_minor_locator(mticker.AutoMinorLocator())
